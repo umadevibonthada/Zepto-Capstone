0b358faa7b5ccc28f182a298f53851fd7e8f4c3d
umadevibonthada <umadevibonthada@gmail.com>
2026-09-22 00:18:46 +0530

    Inspect available product fields

--- data_pipeline/scraper.py
+++ data_pipeline/scraper.py
@@ -5,7 +5,15 @@ response=requests.get(url)
 #print(response.status_code)
 soup = BeautifulSoup(response.text, "html.parser")
 print(soup.title.text)
-book = soup.find("article", class_="product_pod")
-#book = soup.find("article", class_="product_pod")
 
-print(book.h3.a["title"])
+#books=soup.find_all("article",class_="product_pod")
+book = soup.find("article", class_="product_pod")
+
+print(book.prettify())
+
+#print(book.h3.a["title"])
+#print(book.prettify())
+#print("Number of books:", len(books))
+
+for element in book.find_all():
+    print(element.name, element.get("class"), element.get("href"))
